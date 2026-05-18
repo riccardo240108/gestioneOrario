4814a52293a33612dbde523573c39bbad40caee2
riccardo240108 <r.fae.712@liceorussell.eu>
2026-05-18 13:47:03 +0200

    Add files via upload

---
+++ scuola1.sql
@@ -0,0 +1,274 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Creato il: Mag 18, 2026 alle 13:46
+-- Versione del server: 10.4.28-MariaDB
+-- Versione PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `scuola1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `classi`
+--
+
+CREATE TABLE `classi` (
+  `nome_classe` varchar(10) NOT NULL,
+  `indirizzo` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `classi`
+--
+
+INSERT INTO `classi` (`nome_classe`, `indirizzo`) VALUES
+('1A', 'Scientifico_biennio_comune'),
+('1B', 'Scientifico_biennio_comune'),
+('1C', 'Scientifico_biennio_comune'),
+('1D', 'Scientifico_biennio_comune'),
+('1E', 'Scientifico_biennio_comune'),
+('2A', 'Scientifico_biennio_comune'),
+('2B', 'Scientifico_biennio_comune'),
+('2C', 'Scientifico_biennio_comune'),
+('2D', 'Scientifico_biennio_comune'),
+('2E', 'Scientifico_biennio_comune'),
+('3A', 'Scientifico_doppiaLingua'),
+('3B', 'Scientifico_tradizionale'),
+('3C', 'Scientifico_scienze_applicate'),
+('3D', 'Scientifico_scienze_applicate'),
+('3E', 'Scientifico_scienze_applicate'),
+('4A', 'Scientifico_doppiaLingua'),
+('4B', 'Scientifico_tradizionale'),
+('4C', 'Scientifico_scienze_applicate'),
+('4D', 'Scientifico_scienze_applicate'),
+('4E', 'Scientifico_scienze_applicate'),
+('5A', 'Scientifico_doppiaLingua'),
+('5B', 'Scientifico_tradizionale'),
+('5C', 'Scientifico_scienze_applicate'),
+('5D', 'Scientifico_scienze_applicate'),
+('5E', 'Scientifico_scienze_applicate');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `docenti`
+--
+
+CREATE TABLE `docenti` (
+  `id` int(11) NOT NULL,
+  `cognome` varchar(100) DEFAULT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `ore_contratto` int(11) DEFAULT NULL,
+  `tipo_contratto` enum('di ruolo','sostituto','non di ruolo') DEFAULT 'di ruolo',
+  `sostituisce_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `docenti`
+--
+
+INSERT INTO `docenti` (`id`, `cognome`, `nome`, `email`, `ore_contratto`, `tipo_contratto`, `sostituisce_id`) VALUES
+(1, 'Agostini', 'Nadia', 'agostini.nadia@liceorussell.eu', 9, 'di ruolo', NULL),
+(2, 'Andaloro', 'Stefano', 'andaloro.stefano@liceorussell.eu', 18, 'di ruolo', NULL),
+(3, 'Angeli', 'Livio', 'angeli.livio@liceorussell.eu', 18, 'di ruolo', NULL),
+(4, 'Anniciello', 'Patrizia', 'anniciello.patrizia@liceorussell.eu', 18, 'di ruolo', NULL),
+(5, 'Apolloni', 'Giovanna', 'apolloni.giovanna@liceorussell.eu', 18, 'di ruolo', NULL),
+(6, 'Bandera', 'William', 'bandera.william@liceorussell.eu', 18, 'di ruolo', NULL),
+(7, 'Bernardinatti', 'Barbara', 'bernardinatti.barbara@liceorussell.eu', 18, 'di ruolo', NULL),
+(8, 'Bertolini', 'Claudia', 'bertolini.claudia@liceorussell.eu', 18, 'di ruolo', NULL),
+(9, 'Blasiol', 'Marcella', 'blasiol.marcella@liceorussell.eu', 18, 'di ruolo', NULL),
+(10, 'Brentari', 'Elena', 'brentari.elena@liceorussell.eu', 18, 'di ruolo', NULL),
+(11, 'Brugnara', 'Roberto', 'brugnara.roberto@liceorussell.eu', 18, 'di ruolo', NULL),
+(12, 'Brunetta', 'Biagio', 'brunetta.biagio@liceorussell.eu', 18, 'di ruolo', NULL),
+(13, 'Callovi', 'Pietro', 'callovi.pietro@liceorussell.eu', 18, 'di ruolo', NULL),
+(14, 'Campillo', 'Maria Cecilia', 'campillo.mariacecilia@liceorussell.eu', 10, 'di ruolo', NULL),
+(15, 'Carrara', 'Claretta', 'carrara.claretta@liceorussell.eu', 18, 'di ruolo', NULL),
+(16, 'Casagrande', 'Chiara', 'casagrande.chiara@liceorussell.eu', 18, 'di ruolo', NULL),
+(17, 'Cattani', 'Michele', 'cattani.michele@liceorussell.eu', 18, 'di ruolo', NULL),
+(18, 'Cescatti', 'Lucia', 'cescatti.lucia@liceorussell.eu', 18, 'di ruolo', NULL),
+(19, 'Chini', 'Stefania', 'chini.stefania@liceorussell.eu', 18, 'di ruolo', NULL),
+(20, 'Ciaghi', 'Cristina', 'ciaghi.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(21, 'Colombo', 'Marco', 'colombo.marco@liceorussell.eu', 18, 'di ruolo', NULL),
+(22, 'Dalpiaz', 'Monica', 'dalpiaz.monica@liceorussell.eu', 18, 'di ruolo', NULL),
+(23, 'De Carli', 'Michele', 'decarli.michele@liceorussell.eu', 18, 'di ruolo', NULL),
+(24, 'Degiampietro', 'Marta', 'degiampietro.marta@liceorussell.eu', 18, 'di ruolo', NULL),
+(25, 'Delladio', 'Daniela', 'delladio.daniela@liceorussell.eu', 18, 'di ruolo', NULL),
+(26, 'Dorigatti', 'Cristina', 'dorigatti.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(27, 'Eccher', 'Cristina', 'eccher.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(28, 'Endrizzi', 'Michela', 'endrizzi.michela@liceorussell.eu', 18, 'di ruolo', NULL),
+(29, 'Fabbri', 'Claudio', 'fabbri.claudio@liceorussell.eu', 18, 'di ruolo', NULL),
+(30, 'Facchinelli', 'Cristina', 'facchinelli.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(31, 'Fait', 'Paola', 'fait.paola@liceorussell.eu', 18, 'di ruolo', NULL),
+(32, 'Fedrizzi', 'Paola', 'fedrizzi.paola@liceorussell.eu', 18, 'di ruolo', NULL),
+(33, 'Ferrari', 'Francesca', 'ferrari.francesca@liceorussell.eu', 18, 'di ruolo', NULL),
+(34, 'Filippi', 'Silvia', 'filippi.silvia@liceorussell.eu', 18, 'di ruolo', NULL),
+(35, 'Fontana', 'Michela', 'fontana.michela@liceorussell.eu', 18, 'di ruolo', NULL),
+(36, 'Franceschini', 'Giulia', 'franceschini.giulia@liceorussell.eu', 18, 'di ruolo', NULL),
+(37, 'Gasperini', 'Cristina', 'gasperini.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(38, 'Giovanazzi', 'Daniela', 'giovanazzi.daniela@liceorussell.eu', 18, 'di ruolo', NULL),
+(39, 'Gubert', 'Manuela', 'gubert.manuela@liceorussell.eu', 18, 'di ruolo', NULL),
+(40, 'Iob', 'Elisa', 'iob.elisa@liceorussell.eu', 18, 'di ruolo', NULL),
+(41, 'Leonardi', 'Paola', 'leonardi.paola@liceorussell.eu', 18, 'di ruolo', NULL),
+(42, 'Maestri', 'Laura', 'maestri.laura@liceorussell.eu', 18, 'di ruolo', NULL),
+(43, 'Maffeis', 'Chiara', 'maffeis.chiara@liceorussell.eu', 18, 'di ruolo', NULL),
+(44, 'Marchi', 'Cristina', 'marchi.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(45, 'Margaritelli', 'Luca', 'margaritelli.luca@liceorussell.eu', 18, 'di ruolo', NULL),
+(46, 'Marini', 'Francesca', 'marini.francesca@liceorussell.eu', 18, 'di ruolo', NULL),
+(47, 'Martinelli', 'Francesca', 'martinelli.francesca@liceorussell.eu', 18, 'di ruolo', NULL),
+(48, 'Micheli', 'Paola', 'micheli.paola@liceorussell.eu', 18, 'di ruolo', NULL),
+(49, 'Miorandi', 'Cristina', 'miorandi.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(50, 'Molon', 'Paola', 'molon.paola@liceorussell.eu', 18, 'di ruolo', NULL),
+(51, 'Morelli', 'Cristina', 'morelli.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(52, 'Mosca', 'Daniela', 'mosca.daniela@liceorussell.eu', 18, 'di ruolo', NULL),
+(53, 'Nardelli', 'Elena', 'nardelli.elena@liceorussell.eu', 18, 'di ruolo', NULL),
+(54, 'Nones', 'Cristina', 'nones.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(55, 'Pancheri', 'Paola', 'pancheri.paola@liceorussell.eu', 18, 'di ruolo', NULL),
+(56, 'Pellegrini', 'Daniela', 'pellegrini.daniela@liceorussell.eu', 18, 'di ruolo', NULL),
+(57, 'Piffer', 'Cristina', 'piffer.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(58, 'Pisoni', 'Daniela', 'pisoni.daniela@liceorussell.eu', 18, 'di ruolo', NULL),
+(59, 'Rizzi', 'Cristina', 'rizzi.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(60, 'Rossi', 'Paola', 'rossi.paola@liceorussell.eu', 18, 'di ruolo', NULL),
+(61, 'Sartori', 'Cristina', 'sartori.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(62, 'Tamanini', 'Daniela', 'tamanini.daniela@liceorussell.eu', 18, 'di ruolo', NULL),
+(63, 'Tomasi', 'Cristina', 'tomasi.cristina@liceorussell.eu', 18, 'di ruolo', NULL),
+(64, 'Zanella', 'Paola', 'zanella.paola@liceorussell.eu', 18, 'di ruolo', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `materie`
+--
+
+CREATE TABLE `materie` (
+  `nome_materia` varchar(100) NOT NULL,
+  `codice_edt` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `materie`
+--
+
+INSERT INTO `materie` (`nome_materia`, `codice_edt`) VALUES
+('Diritto ed economia', 'DIREC'),
+('Diritto ed economia politica', 'DIRITT'),
+('Disegno e storia dell\'arte', 'DIS'),
+('Filosofia', 'FIL'),
+('Fisica', 'FIS'),
+('Lingua e cultura greca', 'LCULTGRECA'),
+('Lingua e cultura latina', 'LAT'),
+('Lingua e cultura straniera francese', 'FRA'),
+('Lingua e cultura straniera inglese', 'ING'),
+('Lingua e cultura straniera spagnola', 'SPA'),
+('Lingua e cultura straniera tedesca', 'TED'),
+('Lingua e letteratura italiana', 'ITA'),
+('Lingua latina', 'LIN'),
+('Matematica', 'MAT'),
+('Scienze naturali (biologia, chimica, scienze della terra)', 'SCINat'),
+('Scienze umane', 'SCIUma'),
+('Storia', 'STO'),
+('Storia dell\'arte', 'STOArte'),
+('Storia e geografia', 'STOGEO');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `piani_studi`
+--
+
+CREATE TABLE `piani_studi` (
+  `indirizzo` varchar(100) NOT NULL,
+  `anno` int(11) NOT NULL,
+  `codice_materia` varchar(20) NOT NULL,
+  `ore_settimanali` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `piani_studi`
+--
+
+INSERT INTO `piani_studi` (`indirizzo`, `anno`, `codice_materia`, `ore_settimanali`) VALUES
+('Classico', 1, 'GRE', 4),
+('Classico', 1, 'ITA', 5),
+('Classico', 1, 'LAT', 5),
+('Classico', 2, 'GRE', 4),
+('Classico', 2, 'ITA', 5),
+('Classico', 2, 'LAT', 5),
+('Classico', 3, 'GRE', 3),
+('Classico', 3, 'ITA', 4),
+('Classico', 3, 'LAT', 4),
+('Classico', 4, 'GRE', 3),
+('Classico', 4, 'ITA', 5),
+('Classico', 4, 'LAT', 4),
+('Classico', 5, 'GRE', 3),
+('Classico', 5, 'ITA', 4),
+('Classico', 5, 'LAT', 4);
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `classi`
+--
+ALTER TABLE `classi`
+  ADD PRIMARY KEY (`nome_classe`);
+
+--
+-- Indici per le tabelle `docenti`
+--
+ALTER TABLE `docenti`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sostituisce_id` (`sostituisce_id`);
+
+--
+-- Indici per le tabelle `materie`
+--
+ALTER TABLE `materie`
+  ADD PRIMARY KEY (`nome_materia`);
+
+--
+-- Indici per le tabelle `piani_studi`
+--
+ALTER TABLE `piani_studi`
+  ADD PRIMARY KEY (`indirizzo`,`anno`,`codice_materia`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `docenti`
+--
+ALTER TABLE `docenti`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `docenti`
+--
+ALTER TABLE `docenti`
+  ADD CONSTRAINT `docenti_ibfk_1` FOREIGN KEY (`sostituisce_id`) REFERENCES `docenti` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
